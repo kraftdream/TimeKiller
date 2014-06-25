f8f498c1007515be21ed13ea504fa34923eb0171
Udod Denis <kraftdream@gmail.com>
2014-06-25 16:38:12 +0300

    Add GUI elements

---
+++ Assets/Scripts/GUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI : MonoBehaviour {
+
+    public int score;
+    public int killCoount;
+    private Time timer;
+
+	void Start () {
+        GameObject score = new GameObject("Score");
+        score.transform.parent = transform;
+
+        score.AddComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
