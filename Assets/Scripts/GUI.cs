2e453b0127a24cc41551ae0bffb726b9ae20f08f
Udod Denis <kraftdream@gmail.com>
2014-06-26 18:01:17 +0300

    Add GUI text "Score" & "Combo"

--- Assets/Scripts/GUI.cs
+++ Assets/Scripts/GUI.cs
@@ -1,21 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GUI : MonoBehaviour {
+public class GUI: MonoBehaviour
+{
+    public class Margins
+    {
+        public float Left { get; set; }
+        public float Top { get; set; }
+        public float Right { get; set; }
+        public float Bottom { get; set; }
 
-    public int score;
-    public int killCoount;
-    private Time timer;
+        public Margins(float _left, float _top, float _right, float _bottom)
+        {
+            Left = _left;
+            Top = _top;
+            Right = _right;
+            Bottom = _bottom;
+        }
+    }
 
-	void Start () {
-        GameObject score = new GameObject("Score");
-        score.transform.parent = transform;
+    public enum ScreenPossition
+    {
+        TopLeft,
+        TopCenter,
+        TopRight,
+        MiddleLeft,
+        MiddleCenter,
+        MiddleRight,
+        BottonLeft,
+        BottonMiddle,
+        BottonRight
+    }
 
-        score.AddComponent<TextMesh>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public ScreenPossition DefaultScreenPossition {
+        get { return ScreenPossition.MiddleCenter; }
+    }
+
+    public static void SetTextScreenPosition(ScreenPossition _screenPossition, GameObject _gameObject)
+    {
+        Vector3 screenSize = GetScreenSizeInWorldPoint();
+        Vector3 textSize = _gameObject.GetComponent<MeshRenderer>().bounds.size;
+        
+        switch(_screenPossition) {
+            case ScreenPossition.TopLeft:
+                _gameObject.transform.localPosition = new Vector3(textSize.x / 2 - screenSize.x, screenSize.y - textSize.y / 2, 0f);
+                break;
+
+            case ScreenPossition.TopCenter:
+                _gameObject.transform.localPosition = new Vector3(0, screenSize.y - textSize.y / 2, 0f);
+                break;
+
+            case ScreenPossition.TopRight:
+                _gameObject.transform.localPosition = new Vector3(screenSize.x - textSize.x / 2, screenSize.y - textSize.y / 2, 0);
+                break;
+
+            case ScreenPossition.MiddleLeft:
+                _gameObject.transform.localPosition = new Vector3(textSize.x / 2 - screenSize.x, 0, 0f);
+                break;
+
+            case ScreenPossition.MiddleCenter:
+                _gameObject.transform.localPosition = new Vector3(0, 0, 0f);
+                break;
+
+            case ScreenPossition.MiddleRight:
+                _gameObject.transform.localPosition = new Vector3(screenSize.x - textSize.x / 2, 0, 0);
+                break;
+
+            case ScreenPossition.BottonLeft:
+                _gameObject.transform.localPosition = new Vector3(textSize.x / 2 - screenSize.x, textSize.y / 2 - screenSize.y, 0f);
+                break;
+
+            case ScreenPossition.BottonMiddle:
+                _gameObject.transform.localPosition = new Vector3(0, textSize.y / 2 - screenSize.y, 0f);
+                break;
+
+            case ScreenPossition.BottonRight:
+                _gameObject.transform.localPosition = new Vector3(screenSize.x - textSize.x / 2, textSize.y / 2 - screenSize.y, 0);
+                break;
+        }
+    }
+
+    public static void SetMargins(Margins _guiMargins, GameObject _gameObject)
+    {
+        _gameObject.transform.localPosition = new Vector3(_gameObject.transform.localPosition.x + (_guiMargins.Left - _guiMargins.Right), _gameObject.transform.localPosition.y + (_guiMargins.Bottom - _guiMargins.Top), 0);
+    }
+
+    private static Vector3 GetScreenSizeInWorldPoint()
+    {
+        return Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+    }
 }
---
+++ Assets/Scripts/GUIText.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class GUIText: GUI
+{
+    private const int GUI_LAYER = 8;
+
+    private GameObject _textObject;
+    private TextMesh _textMesh;
+    private ScreenPossition _currentScreenPosition;
+
+    [SerializeField]
+    private Font _textFont;
+    [SerializeField]
+    private int _fontSize;
+    [SerializeField]
+    private string _textObjectName;
+    [SerializeField]
+    private ScreenPossition _screenPosition;
+    [SerializeField]
+    private string _valueText;
+
+    [SerializeField]
+    private float _marginLeft;
+    [SerializeField]
+    private float _marginTop;
+    [SerializeField]
+    private float _marginRight;
+    [SerializeField]
+    private float _marginBottom;
+
+
+    public ScreenPossition ScreenPosition
+    {
+        get { return _screenPosition; }
+        set { _screenPosition = value; }
+    }
+
+    public string ValueText
+    {
+        get { return _valueText; }
+        set { _valueText = value; }
+    }
+
+    void Awake ()
+    {
+        InitTextObject();
+        SetTextScreenPosition(ScreenPosition, _textObject);
+        SetMargins(new Margins(_marginLeft, _marginTop, _marginRight, _marginBottom), _textObject);
+	}
+
+    void Update()
+    {
+        UpdateScreenPosition();
+    }
+
+    void InitTextObject()
+    {
+        _textObject = new GameObject(_textObjectName);
+        _textObject.transform.parent = transform;
+        _textObject.layer = GUI_LAYER;
+        _textMesh = _textObject.AddComponent<TextMesh>();
+
+        _textMesh.font = _textFont;
+        _textMesh.renderer.material = _textFont.material;
+        _textMesh.characterSize = 0.1f;
+        _textMesh.fontSize = _fontSize;
+        _textMesh.anchor = TextAnchor.MiddleCenter;
+        SetText(ValueText);
+    }
+
+    void SetText(String _text)
+    {
+        _textMesh.text = _text;
+    }
+
+    void UpdateScreenPosition()
+    {
+        if (!_currentScreenPosition.Equals(ScreenPosition))
+        {
+            _currentScreenPosition = ScreenPosition;
+            SetTextScreenPosition(ScreenPosition, _textObject);
+            SetMargins(new Margins(_marginLeft, _marginTop, _marginRight, _marginBottom), _textObject);
+        }
+    }
+}
