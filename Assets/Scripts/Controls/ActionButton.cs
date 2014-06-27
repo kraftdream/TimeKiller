9ee3bbc0f308d579e71d0a6b185eac70e93095c7
BernEugen <berneugen@gmail.com>
2014-06-27 09:49:05 +0300

    Added main scene

---
+++ Assets/Scripts/Controls/ActionButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionButton : MonoBehaviour
+{
+    public Texture2D _texture;
+    private GUIContent _guiContent;
+    private Rect _buttonRect;
+
+    void Start()
+    {
+        _guiContent = new GUIContent {image = _texture};
+        _buttonRect = new Rect(Screen.width - 150, Screen.height - 150, 100, 100);
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(_buttonRect, _texture))
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    private Rect _gameName;
+    private Rect _gameStart;
+    private Rect _gameOptions;
+    private Rect _gameExit;
+
+    public GUIStyle gameNameStyle;
+    public GUIStyle gameButtonsStyle;
+	
+	void Start () {
+	    _gameName = new Rect(Screen.width / 2, Screen.height / 2 - 150, 10, 10);
+        _gameStart = new Rect(Screen.width / 2 - 60 , Screen.height / 2, 160, 50);
+        _gameOptions = new Rect(Screen.width / 2 - 60, Screen.height / 2 + 80, 160, 50);
+        _gameExit = new Rect(Screen.width / 2 - 60, Screen.height / 2 + 160, 160, 50);
+	}
+	
+	void OnGUI () {
+        
+        GUI.Label(_gameName, "Time    Killer", gameNameStyle);
+	    GUI.skin.button = gameButtonsStyle;
+
+        if (GUI.Button(_gameStart, "Start"))
+	    {
+            Application.LoadLevel("GameScene");
+	    }
+
+        if (GUI.Button(_gameOptions, "Options"))
+        {
+            
+        }
+
+        if (GUI.Button(_gameExit, "Exit"))
+        {
+            Application.Quit();
+        }
+	}
+}
