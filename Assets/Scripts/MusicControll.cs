59cad6414724cc7d3ea9fed23e69cd74c865d97c
Daniilys <daniil.pozhidaev@gmail.com>
2014-07-10 11:25:01 +0300

    fix

---
+++ Assets/Scripts/MusicControll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicControll : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+	void Awake () {
+        audioSource = GetComponent<AudioSource>();
+
+        if (PlayerPrefs.GetString("Music") == "On")
+        {
+            audioSource.Play();
+        }
+	}
+}
