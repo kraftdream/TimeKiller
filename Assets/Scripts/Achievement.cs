d4db084bb937a069d96d9690f5a2bf81976de362
Daniilys <daniil.pozhidaev@gmail.com>
2014-07-23 10:57:05 +0300

    ahievements

---
+++ Assets/Scripts/Achievement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public abstract  class Achievement
+{
+    protected HeroControll _hero;
+    protected String _name;
+    protected int _progress;
+    protected int _currentProgress;
+    protected bool _unlocked;
+
+    public Achievement(String name, int progress, HeroControll hero)
+    {
+        _name = name;
+        _progress = progress;
+        _hero = hero;
+    }
+
+    public bool AddProgress(int currentProgress)
+    {
+        _currentProgress += currentProgress;
+        if (_currentProgress >= _progress)
+        {
+            _currentProgress = 0;
+            return true;
+        }
+        return false;
+    }
+
+    public String GetName()
+    {
+        return _name;
+    }
+
+    public abstract void AddBonus();
+}
+
+public class KillVampires : Achievement
+{
+    public KillVampires(String name, int progress, HeroControll hero):base(name, progress, hero)
+    {}
+
+    public override void AddBonus()
+    {   
+        _hero.BonusCombo = 2;
+        _hero.Invoke("RemoveBonuses", 1);
+    }
+}
+
+
---
+++ Assets/Scripts/AchievementsManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms;
+
+public class AchievementsManager : MonoBehaviour
+{
+    public HeroControll hero;
+    private const String LOCKED = "Locked";
+    private const String UNLOCKED = "Unlocked";
+    private Achievement[] achievements;
+    public delegate void OnAchievementUnlocked (Achievement achievement);
+    public event OnAchievementUnlocked AchievementUnlocked;
+
+    private static AchievementsManager instance;
+ 
+    void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        achievements = new []{ new KillVampires("KillVampires", 5, hero) };
+    }
+
+    public static AchievementsManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void UnlockAchievement(Achievement achievement)
+    {
+        String achievementName = achievement.GetName();
+        if (!PlayerPrefs.HasKey(achievementName) && PlayerPrefs.GetString(achievementName) == LOCKED)
+        {
+            PlayerPrefs.SetString(achievementName, UNLOCKED);
+            AchievementUnlocked(achievement);
+        }
+    }
+
+    private Achievement GetAchievementByName(string achievementName)
+    {
+        return achievements.FirstOrDefault(achievement => achievement.GetName() == achievementName);
+    }
+
+    public void ReportAchievementProgress(string achivementName, int progress)
+    {
+        Achievement achivement = GetAchievementByName(achivementName);
+        if (achivement.AddProgress(progress))
+        {
+            achivement.AddBonus();
+            UnlockAchievement(achivement);
+        }
+    }
+}
